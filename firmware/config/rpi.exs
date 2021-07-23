352242daf5f0200064cd2d5f87aba3d476c69e4f
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-07-23 10:35:22 +0200

    Added monitor specific configuration options to boot image.

--- firmware/config/rpi.exs
+++ firmware/config/rpi.exs
@@ -3,6 +3,8 @@ use Mix.Config
 # Target config for RPi
 # Used even for RPi0 to set the main USB port to non-gadget mode, so that the touchscreen can send touch inputs!
 
+config :nerves, :firmware, fwup_conf: "config/rpi/fwup.conf"
+
 config :hello_nerves, :viewport, %{
   name: :main_viewport,
   # default_scene: {HelloNerves.Scene.Crosshair, nil},
