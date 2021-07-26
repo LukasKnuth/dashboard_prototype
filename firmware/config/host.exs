f1e2eae7d63fb4a8df5fcd28a00d78f39cadef63
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-07-26 22:29:05 +0200

    Configure host to show scenic as well.

--- firmware/config/host.exs
+++ firmware/config/host.exs
@@ -1,3 +1,17 @@
 import Config
 
 # Add configuration that is only needed when running on the host here.
+
+config :hello_nerves, :viewport, %{
+  name: :main_viewport,
+  default_scene: {HelloNerves.Scene.Test, nil},
+  size: {1024, 600},
+  opts: [scale: 1.0],
+  drivers: [
+    %{
+      module: Scenic.Driver.Glfw,
+      name: :glfw,
+      opts: [resizeable: false, title: "Dashboard (host)"],
+    }
+  ]
+}
