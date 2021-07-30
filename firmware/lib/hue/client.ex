1f5001c7ce7d8f120707c761fdfc0f5668fb8b26
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-07-30 20:14:21 +0200

    Added toggle method to Hue client.

--- firmware/lib/hue/client.ex
+++ firmware/lib/hue/client.ex
@@ -3,6 +3,8 @@ defmodule Hue.Client do
 
   alias Hue.{Group, Light}
 
+  # todo how can this be tested? Keep this requests only mock completely? or can we write MockAdapter?
+
   @bridge_ip "192.168.178.27"
   @user_name "6E8WkUgDB1PTrnaD1nMmdRbLMwWrid49NVsZrHYJ"
 
@@ -11,6 +13,10 @@ defmodule Hue.Client do
 
   def groups do
     get("/groups")
+  end
+
+  def group(id) do
+    get("/groups/#{id}")
   end
 
   def group_on(id, set_on) do
@@ -29,8 +35,30 @@ defmodule Hue.Client do
     get("/lights")
   end
 
+  def light(id) do
+    get("/lights/#{id}")
+  end
+
   def light_on(id, set_on) do
     put("/lights/#{id}/state", %{on: set_on})
+  end
+
+  @type toggleable_item :: %Group{} | %Light{}
+  @spec toggle(toggleable_item()) :: {:ok, toggleable_item()} | {:error, any()}
+  def toggle(item)
+
+  def toggle(%Light{id: id, is_on?: state}) do
+    with {:ok, _} <- light_on(id, !state),
+         {:ok, %{body: body}} <- light(id) do
+      {:ok, Light.from_response(id, body)}
+    end
+  end
+
+  def toggle(%Group{id: id, all_on?: state}) do
+    with {:ok, _} <- group_on(id, !state),
+         {:ok, %{body: body}} <- group(id) do
+      {:ok, Group.from_response(id, body)}
+    end
   end
 
   @type refreshable_item :: %Group{} | %Light{}
@@ -38,13 +66,13 @@ defmodule Hue.Client do
   def refresh(item)
 
   def refresh(%Group{id: id}) do
-    with {:ok, %{body: body}} <- get("/groups/#{id}") do
+    with {:ok, %{body: body}} <- group(id) do
       {:ok, Group.from_response(id, body)}
     end
   end
 
   def refresh(%Light{id: id}) do
-    with {:ok, %{body: body}} <- get("/lights/#{id}") do
+    with {:ok, %{body: body}} <- light(id) do
       {:ok, Light.from_response(id, body)}
     end
   end
